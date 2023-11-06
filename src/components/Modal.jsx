33273c66e73a0ebae0e190bb07b1309582cef766
bora <nhatbaoxxd@gmail.com>
2023-11-06 23:50:17 +0700

    Use variant prop to reusing animation states

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,16 +7,18 @@ export default function Modal({ title, children, onClose }) {
       <motion.div
         className="backdrop"
         onClick={onClose}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
+        variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
       />
       <motion.dialog
         open
         className="modal"
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 30 }}
+        variants={{ hidden: { opacity: 0, y: 30 }, visible: { opacity: 1, y: 0 } }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
       >
         <h2>{title}</h2>
         {children}
