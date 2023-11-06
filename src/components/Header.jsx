7da5039e74e4766f1b50f265487877f34262a581
bora <nhatbaoxxd@gmail.com>
2023-11-06 23:39:11 +0700

    Add whileHover effect to the button on hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 import NewChallenge from './NewChallenge.jsx'
 
@@ -22,9 +22,14 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          onClick={handleStartAddNewChallenge}
+          className="button"
+          whileHover={{ scale: 1.05, rotate: 3 }}
+          transition={{ type: 'spring', stiffness: 500 }}
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   )
