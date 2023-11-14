eddef419fd660f6ac5d9c041784f0c2ea31442fa
bora <nhatbaoxxd@gmail.com>
2023-11-14 10:24:09 +0700

    Combining Animations With Layout Animations

--- src/components/ChallengeItem.jsx
+++ src/components/ChallengeItem.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 import { ChallengesContext } from '../store/challenges-context.jsx'
 
@@ -21,7 +21,7 @@ export default function ChallengeItem({ challenge, onViewDetails, isExpanded }) 
   }
 
   return (
-    <motion.li layout="position" exit={{ y: -30, opacity: 0 }}>
+    <motion.li layout exit={{ y: -30, opacity: 0 }}>
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
@@ -45,12 +45,17 @@ export default function ChallengeItem({ challenge, onViewDetails, isExpanded }) 
               </motion.span>
             </button>
           </p>
-
-          {isExpanded && (
-            <div>
-              <p className="challenge-item-description">{challenge.description}</p>
-            </div>
-          )}
+          <AnimatePresence>
+            {isExpanded && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: 'auto', opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+              >
+                <p className="challenge-item-description">{challenge.description}</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </article>
     </motion.li>
