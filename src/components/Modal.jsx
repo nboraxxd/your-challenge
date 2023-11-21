53ec4f57630dc76554db0154c5cdc1d8801f45a0
bora <nhatbaoxxd@gmail.com>
2023-11-21 20:27:56 +0700

    Animating Element Disappearances / Removal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,10 +2,17 @@ import { createPortal } from 'react-dom'
 import { motion } from 'framer-motion'
 
 export default function Modal({ title, children, onClose }) {
+  // khi Element là portal, muốn tạo animate cho nó khi exit thì wrap AnimatePresence ở nơi nó xuất hiện
   return createPortal(
     <>
-      <div className="backdrop" onClick={onClose} />
-      <motion.dialog initial={{ y: -30, opacity: 0 }} animate={{ y: 0, opacity: 1 }} open className="modal">
+      <motion.div exit={{ opacity: 0 }} className="backdrop" onClick={onClose} />
+      <motion.dialog
+        initial={{ y: -30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -30, opacity: 0 }}
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
