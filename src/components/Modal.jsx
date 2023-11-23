ca6dc31c9587f11281b16ea7b3c26785a3d96adb
bora <nhatbaoxxd@gmail.com>
2023-11-23 12:46:29 +0700

    Reusing Animation States

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,11 +5,16 @@ export default function Modal({ title, children, onClose }) {
   // khi Element là portal, muốn tạo animate cho nó khi exit thì wrap AnimatePresence ở nơi nó xuất hiện
   return createPortal(
     <>
-      <motion.div exit={{ opacity: 0 }} className="backdrop" onClick={onClose} />
+      <motion.div
+        exit={{ opacity: 0 }}
+        className="backdrop"
+        onClick={onClose}
+      />
       <motion.dialog
-        initial={{ y: -30, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: -30, opacity: 0 }}
+        variants={{ hidden: { y: -30, opacity: 0 }, visible: { y: 0, opacity: 1 } }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open
         className="modal"
       >
