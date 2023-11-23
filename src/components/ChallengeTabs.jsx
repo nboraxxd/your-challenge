216c460e3b68e1944b9d291776d04dd3fc314cb8
bora <nhatbaoxxd@gmail.com>
2023-11-23 23:52:33 +0700

    Re-triggering Animations via Keys

--- src/components/ChallengeTabs.jsx
+++ src/components/ChallengeTabs.jsx
@@ -6,7 +6,7 @@ function Tab({ isSelected, onSelect, badgeCaption, children }) {
     <li>
       <button className={isSelected ? 'selected' : undefined} onClick={onSelect}>
         {children}
-        <Badge caption={badgeCaption}></Badge>
+        <Badge key={badgeCaption} caption={badgeCaption}></Badge>
       </button>
       {isSelected && <motion.div layoutId="tab-indicator" className="active-tab-indicator" />}
     </li>
