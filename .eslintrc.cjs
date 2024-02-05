2a1053e8fe304e930df95b3930971d0893f339d2
bora <nhatbaoxxd@gmail.com>
2024-02-05 13:41:30 +0700

    Framer Motion Basics & Fundamentals

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,8 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'no-unused-vars': ['warn', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/prop-types': 'off',
   },
 }
